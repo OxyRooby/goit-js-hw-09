a2f56750624fdd3e17200a4529b44578b0c9d1f9
Oksana Teliupa <oxyrooby@gmail.com>
2023-03-20 22:58:50 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const bodyEl = document.querySelector("body");
+const btnStartEl = document.querySelector("button[data-start]");
+const btnStoptEl = document.querySelector("button[data-stop]");
+const wrapEl = document.querySelector(".wrap");
+
+wrapEl.style.display = "flex";
+wrapEl.style.gap = "20px";
+wrapEl.style.justifyContent = "center";
+
+btnStartEl.style.padding = "18px 36px";
+btnStartEl.style.fontSize = "36px";
+btnStoptEl.style.padding = "18px 36px";
+btnStoptEl.style.fontSize = "36px";
+
+let timerId;
+btnStartEl.addEventListener("click", () => {
+    clearInterval(timerId);
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+btnStoptEl.addEventListener("click", () => {
+    clearInterval(timerId);
+});
