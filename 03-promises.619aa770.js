31224c24fa5a50ec4a93cbeb1d92c625a3774bed
OxyRooby <114576828+OxyRooby@users.noreply.github.com>
2023-03-23 11:24:25 +0000

    Deploying to gh-pages from @ OxyRooby/goit-js-hw-09@b2dc67100c78c3d923e5f4fecab44d183b702445 🚀

---
+++ 03-promises.619aa770.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");e.addEventListener("submit",(t=>{t.preventDefault();const{elements:{delay:n,step:r,amount:s}}=t.currentTarget;e.reset()}));
+//# sourceMappingURL=03-promises.619aa770.js.map
